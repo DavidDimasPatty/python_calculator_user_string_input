e4dabddcbec87217a32657b5a19e34b48996d296
unknown <daviddimas80@gmail.com>
2022-04-13 17:00:39 +0700

    change method

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-
+from math import *
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
@@ -19,52 +19,88 @@ def send(msg):
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
 
+#Handle Input
 def string_check(inpcheck):
-    brackets=0
-    res=True
-    operator=["+","-","/","*"]
-    num=["0","1","2","3","4","5","6","7","8","9"]
-    for i in range (0,len(inpcheck)):
-        if(i==len(inpcheck)-1 and brackets!=0):
-            res=False
-            break
-        if(inpcheck[i]=='('):
-            brackets=brackets+1
-            if(inpcheck[i-1] in num and i!=0):
-                res=False
-                break
-            if(inpcheck[i+1] in operator and i!=len(inpcheck)-1):
-                res=False
-                break
-        if(inpcheck[i]==')'):
-            brackets=brackets-1
-            if(inpcheck[i+1] in num and i!=len(inpcheck)-1):
-                res=False
-                break
-            if(inpcheck[i-1] in operator and i!=0):
-                res=False
-                break
-        if(inpcheck[i] in operator):
-            if(inpcheck[i-1] in operator and i!=0):
-                res=False
-                break
-            if(inpcheck[i+1] in operator and i!=len(inpcheck)-1):
-                res=False
-                break
-    return res    
+  check=True
+  try:
+    eval(inpcheck)
+  except (SyntaxError,NameError,TypeError,ValueError) as e:
+    check=False
+  return check    
     
+
+#Without variable
+def withoutV():
+    while True:
+        try:
+            print("Input math expressions:")
+            inp_user=input()
+            if(inp_user=="q" or  inp_user=="Q"):       
+                break
+            check_string=string_check(inp_user)
+            if check_string==True:
+                send(inp_user)
+            else:       
+                send("fail")
+        except (SyntaxError,NameError,TypeError,ValueError,ZeroDivisionError) as e:
+            print("Please enter correct format integer")    
+            print()
+            
+#With variable
+def withV():
+    while True:
+        print("Number of variable")   
+        num_variable=0
+        try:
+            try:
+                num_variable=int(input())
+                if(num_variable=="q" or  num_variable=="Q"):       
+                    break
+            except (SyntaxError,NameError,TypeError,ValueError) as e:
+                print("variable number must be Integer")
+                print()
+                break            
+            print()
+            var=[]
+            alp=[]
+            for i in range(1,num_variable+1):
+                number=int(input(f"{chr(65+i-1)} : "))
+                if(number=="q" or  number=="Q"):       
+                        break
+              
+                alp.append(chr(65+i-1))
+                var.append(number)
+            print("Input math expressions:")
+            inp_user=input()
+            for i in range (0,len(alp)):
+                inp_user=inp_user.replace(alp[i],str(var[i]))
+            if(inp_user=="q" or  inp_user=="Q"):       
+                break
+            check_string=string_check(inp_user)
+            if check_string==True:
+                send(inp_user)
+            else:       
+                send("fail")
+        except (SyntaxError,NameError,TypeError,ValueError) as e:
+            print("Please enter correct format integer")
+            print()
+      
     
+#Main    
 print("Hello, thank you for using one line calculator!")
-print("input q to quit calculator")
 while True:
-    print("Input math expressions:")
-    inp_user=input()
-    if(inp_user=="q"):       
-        send(DISCONNECT_MESSAGE)
-        break
-    check_string=string_check(inp_user)
-    if check_string==True:
-        send(inp_user)
-    else:       
-        send("fail")
+    print("Select Mode")
+    print("A) With Variable")
+    print("B) Without Variable")
+    print("(INPUT 'A' or 'B' to process next step )")
+    print("- Input q to quit calculator -")
+    inp_mode=input()
+    if(inp_mode=="B" or inp_mode=="b"):
+        withoutV()
+    elif(inp_mode=="A" or inp_mode=="a"):
+        withV()
+    elif(inp_mode=="q" or  inp_mode=="Q"):    
+            break
+    else:
+         print("Please select mode")    
     
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from re import X
 import socket 
 import threading
 from unittest import result
+from math import *
 
 HEADER = 64
 PORT = 5050
@@ -25,7 +26,7 @@ def handle_client(conn, addr):
             if msg== "fail":
                 conn.send("WRONG INPUT FORMAT! TRY AND CHECK AGAIN".encode(FORMAT))
             else:
-                res=calculator(msg)
+                res=eval(msg)
                 print(res)
                 conn.send(str(res).encode(FORMAT))    
                  
@@ -34,87 +35,8 @@ def handle_client(conn, addr):
                  conn.send(bye.encode(FORMAT))
                  connected = False
 
-            print(f"[{addr}] {msg}")
-            
-            
-
+            print(f" Success Calculator From [{addr}]")
     conn.close()
-
-
-def numparser(Allexp):
-        leng=len(Allexp)
-        print(leng)
-        arrnum=[]
-        num=""
-        x=0
-        for i in range(leng):
-            print(num)
-            if(Allexp[i]=="+" or Allexp[i]=="-" or Allexp[i]=="*" or Allexp[i]=="/" or Allexp[i]=="(" or Allexp[i]==")" or i==leng-1):
-                if(num==""):
-                    num=Allexp[i]
-                elif(Allexp[i]!="+" and Allexp[i]!="-" and Allexp[i]!="*" and Allexp[i]!="/" and Allexp[i]!="(" and Allexp[i]!=")"):
-                    num=num+Allexp[i]
-                print("masuk")
-                arrnum.append(num)
-                num=""
-            if(Allexp[i]!="+" and Allexp[i]!="-" and Allexp[i]!="*" and Allexp[i]!="/" and Allexp[i]!="(" and Allexp[i]!=")"):
-                    num=num+Allexp[i]
-               
-                 
-        return arrnum
-        
-def operatorparser(Allexp):
-        leng=len(Allexp)
-        arrop=[]
-        for i in range(0,leng):
-            if(Allexp[i]=="+" or Allexp[i]=="-" or Allexp[i]=="*" or Allexp[i]=="/" or Allexp[i]=="(" or Allexp[i]==")"):
-                    arrop.append(Allexp[i])
-          
-        return arrop
-
-def checkbranch(exp):
-    res=""
-    for i in range (0,len(exp)):
-        if(exp[i]=="("):
-            x=i+1
-            for x in range (x,len(exp)):
-                if(exp[x]==")"):
-                    print("dalam: "+res)
-                    temp=res
-                    res=calculator(res)
-                    print("hasil bracket "+str(res))
-                    return exp.replace("("+temp+")",str(res))
-                elif(exp[x]=="("):
-                    exp=exp.replace(exp[x:len(exp)],checkbranch(exp[x:len(exp)]))
-                    print("bracket dalam:"+exp)
-                    res=res+exp[x]
-                else:
-                    res=res+exp[x]
-    return exp                
-            
-def calculator(Allexp):
-        leng=len(Allexp)
-        brch=checkbranch(Allexp)
-        if(str(brch).find("(") and str(brch)!=None):
-            str(brch).replace("(","")         
-            str(brch).replace(")","")
-        print("string jadi "+str(brch))
-        Allexp=str(brch)
-        arrnum=numparser(Allexp)
-        print(arrnum)
-        arrop=operatorparser(Allexp)
-        print(arrop)
-        res=arrnum[0]
-        for i in range (0,len(arrop)):
-              if(arrop[i]=="+"):
-                  res=float(res)+float(arrnum[i+1])
-              if(arrop[i]=="-"):
-                  res=float(res)-float(arrnum[i+1])
-              if(arrop[i]=="/"):
-                  res=float(res)/float(arrnum[i+1])
-              if(arrop[i]=="*"):
-                  res=float(res)*float(arrnum[i+1])  
-        return res            
 
 def start():
     server.listen()
@@ -123,7 +45,7 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
 
 
 print("[STARTING] server is starting...")
